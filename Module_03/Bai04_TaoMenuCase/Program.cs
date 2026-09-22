412d552b5dbc54a5a49afc4fdebf62d9d34c608f
Phan Tan Phat <phantanphat1308@gmail.com>
2026-09-22 15:52:58 +0700

    generate code từ EA vào bài 2

---
+++ Module_03/Bai04_TaoMenuCase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bai04_TaoMenuCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
